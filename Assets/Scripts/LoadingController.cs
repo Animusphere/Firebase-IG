488b0e9173ba250c73a1ddc6b3d4eddda6e16381
Ricky Limbaug <54813543+Animusphere@users.noreply.github.com>
2021-11-20 10:24:36 +0700

    saveF
    added local-savef-eature o the game

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start()
+    {
+        UserDataManager.Load();
+        SceneManager.LoadScene(1);
+    }
+
+}
